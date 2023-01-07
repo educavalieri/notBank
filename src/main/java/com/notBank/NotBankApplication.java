033fbb08f0bfd270b7330c39a14ba043f1d6e3bf
educavalieri <eduardocavalieri@hotmail.com>
2023-01-07 17:53:08 -0300

    implement project

---
+++ src/main/java/com/notBank/NotBankApplication.java
@@ -0,0 +1,13 @@
+package com.notBank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotBankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/notBank/NotBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.notBank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
