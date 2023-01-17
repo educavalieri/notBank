b227faef784bb8f6add82b893448862e1b021e9d
educavalieri <eduardocavalieri@hotmail.com>
2023-01-17 19:40:49 -0300

    Implement PostgreSQL configuration

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,10 +1,21 @@
-INSERT INTO "tb_user" ("name", "email", "password") VALUES ('Eduardo', 'eduardo@gmail.com', '$2a$10$eACCYoNOHEqXve8aIWT8Nu3PkMXWBaOxJ9aORUYzfMQCbVBIhZ8tG');
-INSERT INTO "tb_user" ("name", "email", "password") VALUES ('Camila', 'camila@gmail.com', '$2a$10$eACCYoNOHEqXve8aIWT8Nu3PkMXWBaOxJ9aORUYzfMQCbVBIhZ8tG');
-INSERT INTO "tb_user" ("name", "email", "password") VALUES ('Leonardo', 'leonardo@gmail.com', '$2a$10$eACCYoNOHEqXve8aIWT8Nu3PkMXWBaOxJ9aORUYzfMQCbVBIhZ8tG');
-INSERT INTO "tb_user" ("name", "email", "password") VALUES ('Rafael', 'rafael@gmail.com', '$2a$10$eACCYoNOHEqXve8aIWT8Nu3PkMXWBaOxJ9aORUYzfMQCbVBIhZ8tG');
+--INSERT INTO "tb_user" ("name", "email", "password") VALUES ('Eduardo', 'eduardo@gmail.com', '$2a$10$eACCYoNOHEqXve8aIWT8Nu3PkMXWBaOxJ9aORUYzfMQCbVBIhZ8tG');
+--INSERT INTO "tb_user" ("name", "email", "password") VALUES ('Camila', 'camila@gmail.com', '$2a$10$eACCYoNOHEqXve8aIWT8Nu3PkMXWBaOxJ9aORUYzfMQCbVBIhZ8tG');
+--INSERT INTO "tb_user" ("name", "email", "password") VALUES ('Leonardo', 'leonardo@gmail.com', '$2a$10$eACCYoNOHEqXve8aIWT8Nu3PkMXWBaOxJ9aORUYzfMQCbVBIhZ8tG');
+--INSERT INTO "tb_user" ("name", "email", "password") VALUES ('Rafael', 'rafael@gmail.com', '$2a$10$eACCYoNOHEqXve8aIWT8Nu3PkMXWBaOxJ9aORUYzfMQCbVBIhZ8tG');
+--
+--INSERT INTO "tb_role" ("authority") VALUES ('ROLE_ADMIN');
+--INSERT INTO "tb_role" ("authority") VALUES ('ROLE_VISITOR');
+--
+--INSERT INTO "tb_user_role" ("user_id", "role_id") VALUES (1, 1);
+--INSERT INTO "tb_user_role" ("user_id", "role_id") VALUES (2, 2);
 
-INSERT INTO "tb_role" ("authority") VALUES ('ROLE_ADMIN');
-INSERT INTO "tb_role" ("authority") VALUES ('ROLE_VISITOR');
+INSERT INTO tb_user (name, email, password) VALUES ('Eduardo', 'eduardo@gmail.com', '$2a$10$eACCYoNOHEqXve8aIWT8Nu3PkMXWBaOxJ9aORUYzfMQCbVBIhZ8tG');
+INSERT INTO tb_user (name, email, password) VALUES ('Camila', 'camila@gmail.com', '$2a$10$eACCYoNOHEqXve8aIWT8Nu3PkMXWBaOxJ9aORUYzfMQCbVBIhZ8tG');
+INSERT INTO tb_user (name, email, password) VALUES ('Leonardo', 'leonardo@gmail.com', '$2a$10$eACCYoNOHEqXve8aIWT8Nu3PkMXWBaOxJ9aORUYzfMQCbVBIhZ8tG');
+INSERT INTO tb_user (name, email, password) VALUES ('Rafael', 'rafael@gmail.com', '$2a$10$eACCYoNOHEqXve8aIWT8Nu3PkMXWBaOxJ9aORUYzfMQCbVBIhZ8tG');
 
-INSERT INTO "tb_user_role" ("user_id", "role_id") VALUES (1, 1);
-INSERT INTO "tb_user_role" ("user_id", "role_id") VALUES (2, 2);
+INSERT INTO tb_role (authority) VALUES ('ROLE_ADMIN');
+INSERT INTO tb_role (authority) VALUES ('ROLE_VISITOR');
+
+INSERT INTO tb_user_role (user_id, role_id) VALUES (1, 1);
+INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 2);
